7626749e219ae349326e54b6de72c478ea6d0a5c
lirui1 <lirui1@funshion.com>
2015-09-07 08:58:14 +0800

    add config

--- src/utils/SmsSender.py
+++ src/utils/SmsSender.py
@@ -3,31 +3,26 @@
 
 import urllib
 import hashlib
+import json
 
 def md5(str):
     m = hashlib.md5()   
     m.update(str)
     return m.hexdigest()
 
-smsBaseUrl = "http://sdk2.entinfo.cn:8061/webservice.asmx/mdsmssend?%s"
-sn = "SDK-SKY-010-01656"
-pwd = md5(sn + "01656").upper()
-mobile = "15972957263"
+smsConfigFile = file("../../conf/config.json")
+smsConfig = json.load(smsConfigFile)["smsConfig"]
+smsConfigFile.close()
 
-params = {
-          "sn" : "SDK-SKY-010-194947",
-          "pwd" : pwd,
-          "mobile" : "15972957263",
-          "content" : "2222",
-          "ext" : "",
-          "rrid" : "",
-          "stime" : "",
-          "msgfmt" : ""
-          
-    }
+smsBaseUrl = smsConfig["url"] + "?%s"
+smsParams = smsConfig["params"]
+smsParams["pwd"] = md5(smsParams["sn"] + smsParams["pwd"]).upper()
+
+smsParams["content"] = "testhello"
+
 # print urllib.urlencode(params)
-print smsBaseUrl % urllib.urlencode(params)
-f = urllib.urlopen(smsBaseUrl % urllib.urlencode(params))
-print f.read()
+print smsBaseUrl % urllib.urlencode(smsParams)
+response = urllib.urlopen(smsBaseUrl % urllib.urlencode(smsParams))
+print response.read()
 
 
